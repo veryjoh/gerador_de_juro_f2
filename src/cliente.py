0415fdc636cbdc73d07ffed4e250b0c9ed0559c8
GSB <jgs8115@gmail.com>
2021-08-18 12:02:34 -0300

    modifying the tax

--- src/cliente.py
+++ src/cliente.py
@@ -1,8 +1,37 @@
 class Cliente:
 
-    def __init__(self, nome):
+    def __init__(self, nome, cpf, email, telefone):
         self._nome = nome
+        self._cpf = cpf
+        self._email = email
+        self._telefone = telefone
 
     @property
     def nome(self):
         return self._nome
+
+    def alterar_email(self, novo_email):
+        if novo_email and "@" in novo_email:
+            self._email = novo_email
+        else:
+            print("Informe um e-mail válido!")
+    
+    def alterar_telefone(self, novo_telefone):
+        if novo_telefone:
+            self._telefone = novo_telefone
+        else:
+            print("Informe um número válido!")
+
+    def valida_cpf(self):
+        return len(self._cpf == 11)
+
+    def __str__(self) -> str:
+        details = {
+            "Nome":self._nome,
+            "CPF":self._cpf,
+            "E-mail": self._cpf,
+            "Telefone": self._telefone
+        }
+        return details
+
+    
--- src/emprestimo.py
+++ src/emprestimo.py
@@ -3,7 +3,7 @@ class Emprestimo:
     def __init__(self, cliente, valor_solicitado: int, dias_p_devolucao: int):
         self._id = (id(self))
         self._cliente = cliente
-        self._juros_base = 0.60
+        self._juros_base = 0.6
         self._valor_solicitado = int(valor_solicitado)
         self._dias_p_devolucao = int(dias_p_devolucao)
 
@@ -16,7 +16,9 @@ class Emprestimo:
         return self._dias_p_devolucao
 
     def acresce_juros(self):
-        valor_com_juros = self.valor_solicitado*self._juros_base
+        valor_com_juros = (
+            self.valor_solicitado * self._juros_base
+            ) + self.dias_p_devolucao
         return valor_com_juros
 
     def calcula_valor_final(self):
@@ -26,7 +28,7 @@ class Emprestimo:
     def emprestimo_pago(self):
         return True
 
-    def resumo_do_emprestimo(self):
+    def _str__(self):
 
         resumo_do_emprestimo = {
             "Cliente": self._cliente.nome,
